197471d7d61ddad99403ef2a516868b5363500a3
ivan.mihaylovskyy <ivan.mihaylovskyy@pearl.com>
2013-11-13 08:57:27 +0200

    ninject

---
+++ Selenium.HtmlElements.Demo/Injector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Ninject;
+
+using Selenium.HtmlElements.Demo.Modules;
+
+namespace Selenium.HtmlElements.Demo {
+
+    internal static class Injector {
+
+        public static readonly IKernel Kernel;
+
+        static Injector() {
+            Kernel = new StandardKernel(new SettingsModule());
+        }
+
+    }
+
+}
