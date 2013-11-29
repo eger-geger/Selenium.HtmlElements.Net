ea3da998fa13e84665c92e5cb9399950611bef53
ivan.mihaylovskyy <ivan.mihaylovskyy@perl.com>
2013-11-30 01:07:00 +0200

    ready state

--- Selenium.HtmlElements/Elements/HtmlPage.cs
+++ Selenium.HtmlElements/Elements/HtmlPage.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Linq;
 
 using OpenQA.Selenium;
 
@@ -6,8 +8,32 @@ namespace HtmlElements.Elements {
 
     public class HtmlPage : SearchContextWrapper {
 
+        public enum DocumentReadyState {
+
+            Uninitialized,
+            Loading,
+            Interactive,
+            Complete
+
+        }
+
         public HtmlPage(ISearchContext wrapped) : base(wrapped) {
             ElementActivator.Activate(this, wrapped);
+        }
+
+        public DocumentReadyState ReadyState {
+            get { 
+                var readyState = (string) ExecuteScript("return document.readyState;");
+
+                switch (readyState) {
+                    case "uninitialized": return DocumentReadyState.Uninitialized;
+                    case "loading": return DocumentReadyState.Loading;
+                    case "interactive": return DocumentReadyState.Interactive;
+                    case "complete": return DocumentReadyState.Complete;
+                }
+
+                throw new InvalidOperationException("unexpected document state");
+            }
         }
 
         public string Title {
@@ -33,7 +59,6 @@ namespace HtmlElements.Elements {
         public void DeleteAllCoockies() {
             WrappedDriver.Manage().Cookies.DeleteAllCookies();
         }
-
     }
 
 }
