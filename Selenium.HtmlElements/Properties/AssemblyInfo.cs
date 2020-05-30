71d33670a466f3f0bac4aa1ad6ebc2937b0a6bc7
Ivan Mykhaylovskyy <ivan.mykhaylovskyy@justanswer.com>
2020-05-30 16:03:05 +0300

    move nuspec information to project file and remove nuspec file

--- Selenium.HtmlElements/Properties/AssemblyInfo.cs
+++ Selenium.HtmlElements/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Page Object support library for Selenium.WebDriver")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: Guid("df09e424-55f1-4ef4-a17f-26c0dd792ed0")]
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
 
 [assembly: InternalsVisibleTo("HtmlElements.Test")]
 [assembly: InternalsVisibleTo("HtmlElements.IntegrationTests")]
