90e7d9b4b4eff2b624a23dc076385e229c70ce89
Ivan Mykhaylovskyy <ivan.mykhaylovskyy@justanswer.com>
2020-05-30 15:13:44 +0300

    add http lib unit tests

---
+++ Selenium.HtmlElements.Test/src/Utilities/HttpLibTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using HtmlElements.Utilities;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace HtmlElements.Test.Utilities
+{
+    public class HttpLibTests
+    {
+        private static IEnumerable<ITestCaseData> ParseQueryStringTests
+        {
+            get
+            {
+                yield return new TestCaseData(null).Returns(new NameValueCollection());
+                yield return new TestCaseData(string.Empty).Returns(new NameValueCollection());
+                
+                yield return new TestCaseData("?name=Jack&age=14")
+                    .Returns(new NameValueCollectionBuilder()
+                        .Add("name", "Jack")
+                        .Add("age", "14")
+                        .NameValueCollection
+                    );
+                
+                yield return new TestCaseData("?name=Jack&age=")
+                    .Returns(new NameValueCollectionBuilder()
+                        .Add("name", "Jack")
+                        .Add("age", "")
+                        .NameValueCollection
+                    );
+                
+                yield return new TestCaseData("?name=Jack&age")
+                    .Returns(new NameValueCollectionBuilder()
+                        .Add("name", "Jack")
+                        .Add("age", "")
+                        .NameValueCollection
+                    );
+                
+                yield return new TestCaseData("?name=Jack&name=Susan")
+                    .Returns(new NameValueCollectionBuilder()
+                        .Add("name", "Jack")
+                        .Add("name", "Susan")
+                        .NameValueCollection
+                    );
+                
+                yield return new TestCaseData("?price=5%24&discount%20price=4.55%24")
+                    .Returns(new NameValueCollectionBuilder()
+                        .Add("price", "5$")
+                        .Add("discount price", "4.55$")
+                        .NameValueCollection
+                    );
+            }
+        }
+
+        [TestCaseSource(nameof(ParseQueryStringTests))]
+        public NameValueCollection ShouldParseUrlQueryString(string queryString)
+        {
+            return HttpLib.ParseQueryString(queryString);
+        }
+    }
+}
---
+++ Selenium.HtmlElements.Test/src/Utilities/NameValueCollectionBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Specialized;
+
+namespace HtmlElements.Test.Utilities
+{
+    public class NameValueCollectionBuilder
+    {
+        private readonly NameValueCollection _collection = new NameValueCollection();
+
+        public NameValueCollectionBuilder Add(string name, string value)
+        {
+            _collection.Add(name, value);
+            return this;
+        }
+        
+        public NameValueCollection NameValueCollection => new NameValueCollection(_collection);
+        
+    }
+}
