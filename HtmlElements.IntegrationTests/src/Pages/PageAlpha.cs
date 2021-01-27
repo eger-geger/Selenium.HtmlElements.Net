7f19eb60bd4d49efff0b7130b7cfc43a9b0360ab
Alex Bakanov <manych@gmail.com>
2021-01-27 11:48:06 +0800

    selenium 4.0-alpha

--- HtmlElements.IntegrationTests/src/Pages/PageAlpha.cs
+++ HtmlElements.IntegrationTests/src/Pages/PageAlpha.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections.Generic;
 using HtmlElements.Elements;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
+using SeleniumExtras.PageObjects;
 
-namespace HtmlElements.IntegrationTests.Pages {
-
-    public class PageAlpha : HtmlPage {
-
-        public PageAlpha(ISearchContext webDriverOrWrapper) : base(webDriverOrWrapper) {}
-
-        [FindsBy(How = How.CssSelector, Using = "#element-list")]
-        public HtmlElement ElementListContainer { get; private set; }
-
-        [FindsBy(How = How.CssSelector, Using = "#element-list li")]
-        public IList<IWebElement> ElementListItems { get; private set; }
-
-        [FindsBy(How = How.CssSelector, Using = "#element-list li"), CacheLookup]
-        public IList<HtmlElement> CachedElementListItems { get; private set; } 
-
-        [FindsBy(How = How.CssSelector, Using = "iframe")]
-        public PageBeta NonCachedBetaFrame { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = "iframe"), CacheLookup]
-        public PageBeta CachedBetaFrame { get; set; }
+namespace HtmlElements.IntegrationTests.Pages
+{
+  public class PageAlpha : HtmlPage
+  {
+    public PageAlpha(ISearchContext webDriverOrWrapper) : base(webDriverOrWrapper)
+    {
     }
 
+    [FindsBy(How = How.CssSelector, Using = "#element-list")]
+    public HtmlElement ElementListContainer { get; private set; }
+
+    [FindsBy(How = How.CssSelector, Using = "#element-list li")]
+    public IList<IWebElement> ElementListItems { get; private set; }
+
+    [FindsBy(How = How.CssSelector, Using = "#element-list li"), CacheLookup]
+    public IList<HtmlElement> CachedElementListItems { get; private set; }
+
+    [FindsBy(How = How.CssSelector, Using = "iframe")]
+    public PageBeta NonCachedBetaFrame { get; set; }
+
+    [FindsBy(How = How.CssSelector, Using = "iframe"), CacheLookup]
+    public PageBeta CachedBetaFrame { get; set; }
+  }
 }
--- Selenium.HtmlElements/src/Locators/ByFactory.cs
+++ Selenium.HtmlElements/src/Locators/ByFactory.cs
@@ -1,60 +1,71 @@
 ﻿using System;
 using System.Globalization;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
+using SeleniumExtras.PageObjects;
 
-namespace HtmlElements.Locators {
+namespace HtmlElements.Locators
+{
+  internal static class ByFactory
+  {
+    public static By Create(How how, string usingValue, Type customFinderType = null)
+    {
+      if (string.IsNullOrWhiteSpace(usingValue) && how == How.Custom)
+      {
+        throw new ArgumentException(string.Format("[Using] should not be empty when [How={0}]", how));
+      }
 
-    internal static class ByFactory {
+      switch (how)
+      {
+        case How.Id:
+          return By.Id(usingValue);
 
-        public static By Create(How how, string usingValue, Type customFinderType = null) {
+        case How.Name:
+          return By.Name(usingValue);
 
-            if (string.IsNullOrWhiteSpace(usingValue) && how == How.Custom)
-            {
-                throw new ArgumentException(string.Format("[Using] should not be empty when [How={0}]", how));
-            }
-                
-            switch (how) {
-                case How.Id:
-                    return By.Id(usingValue);
-                case How.Name:
-                    return By.Name(usingValue);
-                case How.TagName:
-                    return By.TagName(usingValue);
-                case How.ClassName:
-                    return By.ClassName(usingValue);
-                case How.CssSelector:
-                    return By.CssSelector(usingValue);
-                case How.LinkText:
-                    return By.LinkText(usingValue);
-                case How.PartialLinkText:
-                    return By.PartialLinkText(usingValue);
-                case How.XPath:
-                    return By.XPath(usingValue);
-                case How.Custom:
-                    return Create(customFinderType, usingValue);
-            }
+        case How.TagName:
+          return By.TagName(usingValue);
 
-            throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
-                "Cannot construct [By] from [How={0}], [Using={1}]", how, usingValue));
-        }
+        case How.ClassName:
+          return By.ClassName(usingValue);
 
-        public static By Create(Type customFinderType, string usingValue) {
-            if (customFinderType == null) {
-                throw new ArgumentException("Cannot use [How.Custom] without supplying a [CustomFinderType]");
-            }
+        case How.CssSelector:
+          return By.CssSelector(usingValue);
 
-            if (!customFinderType.IsSubclassOf(typeof(By))) {
-                throw new ArgumentException("[CustomFinderType] must be a descendant of [By] class");
-            }
+        case How.LinkText:
+          return By.LinkText(usingValue);
 
-            return Activator.CreateInstance(customFinderType, usingValue) as By;
-        }
+        case How.PartialLinkText:
+          return By.PartialLinkText(usingValue);
 
-        public static By Create(FindsByAttribute attribute) {
-            return Create(attribute.How, attribute.Using, attribute.CustomFinderType);
-        }
+        case How.XPath:
+          return By.XPath(usingValue);
 
+        case How.Custom:
+          return Create(customFinderType, usingValue);
+      }
+
+      throw new ArgumentException(string.Format(CultureInfo.InvariantCulture,
+          "Cannot construct [By] from [How={0}], [Using={1}]", how, usingValue));
     }
 
+    public static By Create(Type customFinderType, string usingValue)
+    {
+      if (customFinderType == null)
+      {
+        throw new ArgumentException("Cannot use [How.Custom] without supplying a [CustomFinderType]");
+      }
+
+      if (!customFinderType.IsSubclassOf(typeof(By)))
+      {
+        throw new ArgumentException("[CustomFinderType] must be a descendant of [By] class");
+      }
+
+      return Activator.CreateInstance(customFinderType, usingValue) as By;
+    }
+
+    public static By Create(FindsByAttribute attribute)
+    {
+      return Create(attribute.How, attribute.Using, attribute.CustomFinderType);
+    }
+  }
 }
