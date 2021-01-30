7974f5602808781256b853c7bcab6a9af70f9e89
Ivan Mihaylovskyy <ivan.mihaylovskyy@gmail.com>
2021-01-30 15:06:07 +0200

    update assembly version

--- Selenium.HtmlElements/Properties/AssemblyInfo.cs
+++ Selenium.HtmlElements/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("HtmlElements")]
 [assembly: AssemblyProduct("HtmlElements")]
 [assembly: AssemblyDescription("Page Object support library for Selenium.WebDriver")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: Guid("df09e424-55f1-4ef4-a17f-26c0dd792ed0")]
-[assembly: AssemblyVersion("3.2.0")]
-[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
 
 [assembly: InternalsVisibleTo("HtmlElements.Test")]
 [assembly: InternalsVisibleTo("HtmlElements.IntegrationTests")]
